b10190de34ed33b6a9fdca868acdd7ed2363f936
Ashley <apark8496@gmail.com>
2022-06-15 20:01:20 -0400

    working on home page about info/storyline/setting up folder structure for pages

---
+++ src/pages/game.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Home() {
+
+    return (
+        
+        <section class="hero">
+            {/* insert hero through bootstrap??? WELCOME TO CAT TAILS, A TEXT ADVENTURE GAME*/}
+            <div class="about-container">
+                {/* about section */}
+                <aside className='about'>
+        
+                    <p>
+                        On one sleepy, mundane spring day one pet wonders further than they ever believed possible.
+                        Experiencing an epic adventure of a lifetime that is typically only ever told in fairytales.
+                        Join them on their journey of a lifetime in the wilderness.
+                        Roam through mystical forest, meet new friends, search for ancient treasures, and 
+                    <br />
+                    </p>
+                </aside>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
