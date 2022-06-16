8e9c6d9870ba1b3f81ef3610a3f7995a6d5b7729
Ashley <apark8496@gmail.com>
2022-06-15 21:17:11 -0400

    updates on game synopsis section

--- src/pages/game.js
+++ src/pages/game.js
@@ -12,12 +12,18 @@ function Home() {
         
                     <p>
                         On one sleepy, mundane spring day one pet wonders further than they ever believed possible.
-                        Experiencing an epic adventure of a lifetime that is typically only ever told in fairytales.
-                        Join them on their journey of a lifetime in the wilderness.
-                        Roam through mystical forest, meet new friends, search for ancient treasures, and 
-                    <br />
+                        Join them and experience an epic adventure of a lifetime that is typically only ever told in fairytales.
+                        Roam through mystical forest, floating islands, otherwordly caves, and
+                        along the way meet new friends, search for ancient treasures, face fearsome foes, and find your way back home.
+                        <br />
+                        <br />
+                        <br />
+                        Your Journey awaits... 
+                        {/* button to enter and sign up/character menu */}
                     </p>
                 </aside>
+
+                {/* section to show possible pets to pick from? */}
             </div>
         </section>
     );
