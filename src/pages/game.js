1ef49d6cdd042396e4087933afdb7c485571bffe
Ashley <apark8496@gmail.com>
2022-06-20 19:01:53 -0400

    added in comments for what should be included in a character card

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,1 +1,15 @@
 // character selection sep page? or all in one
+
+function Game () {
+
+    // char name
+    // CHARACTER CARD INCLUDES:
+    // import character img
+    // Species
+    // special item (fav toy)
+
+    // Start Adventure Button
+
+
+    // Game
+}
