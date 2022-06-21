14c78ed23c23fa5055c3d3fea84bdf43b06d870d
Ashley <apark8496@gmail.com>
2022-06-20 20:33:52 -0400

    added in basic layout for character cards

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,15 +1,69 @@
-// character selection sep page? or all in one
+import React, { useState } from "react";
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Game () {
 
-    // char name
-    // CHARACTER CARD INCLUDES:
-    // import character img
-    // Species
-    // special item (fav toy)
+    const {character} = useState([
+        {
+            pet: "Momo",
+            imagePath: "Momo",
+            type: "Male Cat",
+            toy: "Stuffed Chicken"
+        },
+        {
+            pet: "Logan",
+            imagePath: "Logan",
+            type: "Male Dog",
+            toy: ""
+        },
+        {
+            pet: "Alice",
+            imagePath: "Alice",
+            type: "Female Cat",
+            toy: ""
+        },
+        {
+            pet: "River",
+            imagePath: "River",
+            type: "Dog",
+            toy: ""
+        },
+        {
+            pet: "Selena",
+            imagePath: "Selena",
+            type: "Female Cat",
+            toy: ""
+        },
+        {
+            pet: "Slash",
+            imagePath: "Slash",
+            type: "Male Lizard",
+            toy: ""
+        },
 
-    // Start Adventure Button
+    ])
 
-
-    // Game
+    return
+    <div>
+        <h2>Character Selection</h2>
+        {/* {character}.map((Game)) */} => (
+            <Row className="cards">
+                    <Col>
+                        <Card className="card" style={{ width: '30rem' }} key={character.pet}>
+                            <Card.Img className="card-img" style={{ width: '30rem' }} src={require(`../../../assets/pet-illustrations/${character.imagePath}.png`)} alt={project.pet} />
+                            <Card.Body className="card-info">
+                                <Card.Title className="text-title">{character.pet}</Card.Title>
+                                <Card.Text className="card-description">{character.type}</Card.Text>
+                                <Card.Text className="card-description">{character.toy}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            ))}
+        </div>
+    )
+        )
+    </div>
 }
